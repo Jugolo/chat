7d83eeb30ab3acfc7dafa5a1d279d5f390db445e
Ronnie <rix17172@gmail.com>
2016-04-01 13:12:18 +0200

    Update server.php

--- include/server.php
+++ include/server.php
@@ -11,9 +11,9 @@ Ajax server need to set varibels and header. This is happens here
 */
 function init_ajax(){
  Ajax::header([
-   "Content-Type"  => "application/json",
-   "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0",
-   "Cache-Control" => ["post-check=0, pre-check=0", false],
-   "Pragma"        => "no-cache",
+   ["Content-Type",  "application/json"],
+   ["Cache-Control", "no-store, no-cache, must-revalidate, max-age=0"],
+   ["Cache-Control", "post-check=0, pre-check=0", false],
+   ["Pragma",        "no-cache"],
  ]);
 }
