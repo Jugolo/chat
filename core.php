6f5e33dd0d72d03a681768ab4c160deccb90f6e5
Ronnie <rix17172@gmail.com>
2016-04-01 12:51:33 +0200

    Update core.php

--- core.php
+++ core.php
@@ -6,7 +6,7 @@ include "multi_include.php";//load multi include to get all file wee need
 
 //if this is NOT cli wee want to set ajax variabel!
 if(!is_cli()){
- ini_ajax();
+ init_ajax();
 }
 
 //finaly wee load and set up the diffrence libary so now wee can start the server and let it running until its goal is done.
