fe46c3f25a6876864487f3024f8be5936bbccee6
Ronnie <rix17172@gmail.com>
2016-04-01 15:20:33 +0200

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,46 +1,16 @@
 <?php
+function database_config(){
+  static $c = null;
 
-class Session{
-   private static $sessions = [];
-   private static $current = null;
-
-   public static create(){
-      //wee need to create a new tokens to the user
-      $token = self::new_token();
-
-      if(self::token_exists($token))
-         return self::create();
-
-      self::$sessions[$tokens] = [
-         "created" => time(),
-         "context" => [],
-      ];
-
-      return $token;
-   }
-
-   public static function set_current($token){
-     if(!self::token_exists($token))
-        return false;
-
-      self::$current = $token;
-   }
-
-   private static token_exists($token){
-      //if it not exists in the session array it not exists at all!
-      if(!empty(self::$sessions[$token]))
-        return true;
-
-      return false;
-   }
-
-   private static function new_token(){
-      $use = "qwertyuioplkjhgfdsazxcvbnm.,?!'-/:;()&@[]{}#%^*+=£$€><~|\_1234567890";
-      $return = "";
-      for($i=0;$i<1001;$i++){
-        $return .= $use[mt_rand(0, count($use)-1)];
-      }
-
-      return $return;
-   }
+  if($c != null)
+   return $c;
+ 
+  include(realpath(dirname(__FILE__)."../../config.php"));
+  return $c = [
+    "host" => $db_host,
+    "user" => $db_user,
+    "pass" => $db_pass,
+    "data" => $db_name,
+    "prefix" => DB_PREFIX,
+  ];
 }
