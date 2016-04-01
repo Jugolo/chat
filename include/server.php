39c77278761467cc5e4d6c7daab0a1ddeb7ddaf1
Ronnie <rix17172@gmail.com>
2016-04-01 13:00:56 +0200

    Update server.php

--- include/server.php
+++ include/server.php
@@ -5,3 +5,15 @@ Start the chat server. It will not send header or other task, it takes the reque
 function server_start(){
 
 }
+
+/**
+Ajax server need to set varibels and header. This is happens here
+*/
+function init_ajax(){
+ Ajax::header([
+   "Content-Type"  => "application/json",
+   "Cache-Control" => "no-store, no-cache, must-revalidate, max-age=0",
+   "Cache-Control" => ["post-check=0, pre-check=0", false],
+   "Pragma"        => "no-cache",
+ ]);
+}
