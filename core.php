fc9417cd074e6cdad62393d4fa66e216128e718a
Ronnie <rix17172@gmail.com>
2016-04-01 12:50:37 +0200

    Update core.php

--- core.php
+++ core.php
@@ -4,5 +4,10 @@ define("IN_CHAT", true);
 
 include "multi_include.php";//load multi include to get all file wee need
 
+//if this is NOT cli wee want to set ajax variabel!
+if(!is_cli()){
+ ini_ajax();
+}
+
 //finaly wee load and set up the diffrence libary so now wee can start the server and let it running until its goal is done.
 server_start();
