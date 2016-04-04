dfac39f9c05261ee5c0b6a119cdebc268aae3af1
Ronnie <rix17172@gmail.com>
2016-04-04 10:50:26 +0200

    Update channel.php

--- include/channel.php
+++ include/channel.php
@@ -10,10 +10,30 @@ class Channel{
     //wee has not cache it yet so wee create it if it exists.
     $sql = Database::query("SELECT * FROM `".table("channels")." WHERE `name`=".Database::qlean($name));
     if($sql->rows() == 1){
-      self::$channels[$name] = $sql->fetch();
+      self::$channels[$name] = new ChannelData($sql->fetch());
       return true;
     }
 
     return false;
+  }
+
+  public static function get($name){
+    if(self::exists($name)){
+      return self::$channels[$name];
+    }
+
+    return null;
+  }
+
+  public function create($name, $title=null){
+
+  }
+}
+
+class ChannelData{
+  private $data;
+
+  public function __construct($data){
+    $this->data = $data;
   }
 }
