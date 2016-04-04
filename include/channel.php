15f7e3e4eba2a806a2805dab957438a5031a3d15
Ronnie <rix17172@gmail.com>
2016-04-04 10:43:09 +0200

    Update channel.php

--- include/channel.php
+++ include/channel.php
@@ -1,1 +1,19 @@
+<?php
 
+class Channel{
+  private static $channles = [];
+
+  public static function exists($name){
+    if(!empty(self::$channels[$name]))
+      return true;
+
+    //wee has not cache it yet so wee create it if it exists.
+    $sql = Database::query("SELECT * FROM `".table("channels")." WHERE `name`=".Database::qlean($name));
+    if($sql->rows() == 1){
+      self::$channels[$name] = $sql->fetch();
+      return true;
+    }
+
+    return false;
+  }
+}
