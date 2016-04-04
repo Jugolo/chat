6cd5b030e62e183e513376d1a6050eea976f0647
Ronnie <rix17172@gmail.com>
2016-04-04 11:45:54 +0200

    Update user.php

--- include/user.php
+++ include/user.php
@@ -8,6 +8,22 @@ class User{
   private $user = [];
 
   public static function get($token){
+     if(self::exists($token)){
+        return self::$user[$token];
+     }
 
+     return false;
   }
+
+  public static function exists($token){
+    if(!empty(self::$user))
+      return true;
+
+     $sql = Database::query("SELECT * FROM `".table("user")."` WHERE `token`=".Database::qlean($token));
+     if($sql->rows()){
+       self::$user[$token] = $sql->fetch();
+       return true;
+     }
+
+     return false;
 }
