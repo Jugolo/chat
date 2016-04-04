8f5cb49c6cd0569c68d7ea54cc482fb507d4544e
Ronnie <rix17172@gmail.com>
2016-04-04 21:07:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,17 @@
 <?php
-include "include/system.php
+include "include/system.php";
 
 function session_init(){
-
+  if(cookie("identify")){
+    //control the cookie to detect errors
+    $sql = dbquery("SELECT * FROM `".DB_PREFIX."chat_session_data` WHERE `token`='".cookie("identify")."'");
+    if(dbrows($sql) == 1){
+      $row = dbarray($sql);
+      if($row["ip"] == $_SERVER['REMOTE_ADDR']){
+          
+      }
+    }
+  }
 }
 
 //<-page start here.
