94881a2e1c0a120c96a12d957e59d1821f349f7e
Ronnie <rix17172@gmail.com>
2016-04-04 14:25:57 +0200

    Update login.php

--- include/command/globel/login.php
+++ include/command/globel/login.php
@@ -1,5 +1,10 @@
 <?php
 
 function globel_login($data){
+  if(!is_cli()){
+     send("ERROR: invald token");
+     return false;
+  }
+  
   send("LOGIN: ".(Session::add_token($data) ? "true" : "false"), true);
 }
