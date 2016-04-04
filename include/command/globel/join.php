ac1a78245e291d3e41e2179c6e0778fed657d6b7
Ronnie <rix17172@gmail.com>
2016-04-04 13:21:54 +0200

    Update join.php

--- include/command/globel/join.php
+++ include/command/globel/join.php
@@ -10,7 +10,7 @@ function join_channel($channel){
    if(preg_match("/^\#([a-zA-Z]*?)$/", $channel)){
       $user = get_user();
       $user->join_channel($channel);
-      send_channel($channel, "JOIN ".$channel.": ".$user->nick());
+      send_channel($channel, "JOIN: ".$user->nick());
    }else{
       send("ERROR: invalid channel on join command");
    }
