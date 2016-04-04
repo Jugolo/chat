d77b55254e68c125a29969618ed2c713332aa02d
Ronnie <rix17172@gmail.com>
2016-04-04 14:17:17 +0200

    Update multi_include.php

--- multi_include.php
+++ multi_include.php
@@ -8,6 +8,12 @@ include("include/server.php");
 include("include/database.php");
 include("include/config.php");
 include("include/session.php");
+include("include/ajax.php");
+include("include/channel.php");
+include("include/user.php");
+include("include/websocket.php");
+
 
 //globel command
 include("include/command/globel/login.php");
+include("include/command/globel/join.php");
